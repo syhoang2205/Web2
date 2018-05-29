ae9c5af342fed61772d855d022ab038d37e29708
syhoang2205 <syhoang2205@gmail.com>
2018-05-30 00:57:06 +0700

    index.js
    sửa lại xíu

--- client/assets/me/index.js
+++ client/assets/me/index.js
@@ -13,7 +13,33 @@ var loadProducts = function() {
     $('.loader').show();
 
     $.ajax({
-        url: 'http://localhost:3000/products?page=' + CUR_PAGE,
+        url: 'http://localhost:80/products?page=' + CUR_PAGE,
+        dataType: 'json',
+        timeout: 10000
+    }).done(function(data) {
+        var source = $('#product-template').html();
+        var template = Handlebars.compile(source);
+        var html = template(data.products);
+        $('#product-list').append(html);
+
+        $('#product-list div[style]').fadeIn(200, function() {
+            $(this).removeAttr('style');
+        });
+
+        CUR_PAGE++;
+        if (data.hasMore === false) {
+            $('#btnMore').hide();
+        }
+
+        $('.loader').hide();
+    });
+};
+
+var loadCategory = function() {
+    $('.loadCate').show();
+
+    $.ajax({
+        url: 'http://localhost:80/products?page=' + CUR_PAGE,
         dataType: 'json',
         timeout: 10000
     }).done(function(data) {
