4ab469bef857678ff7a4e972779a69048123c3df
Nguyễn Đức Hoàng <ndhoang011097@gmail.com>
2018-06-15 22:57:00 +0700

    Update Database
    auto insert and add name input(html)

--- qldaugia.sql
+++ qldaugia.sql
@@ -2,10 +2,10 @@
 -- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th6 04, 2018 lúc 08:06 AM
--- Phiên bản máy phục vụ: 10.1.31-MariaDB
--- Phiên bản PHP: 7.2.3
+-- Máy chủ: 127.0.0.1:3306
+-- Thời gian đã tạo: Th6 15, 2018 lúc 03:55 PM
+-- Phiên bản máy phục vụ: 5.7.21
+-- Phiên bản PHP: 5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,12 +28,14 @@ SET time_zone = "+00:00";
 -- Cấu trúc bảng cho bảng `danhmucsp`
 --
 
-CREATE TABLE `danhmucsp` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `danhmucsp`;
+CREATE TABLE IF NOT EXISTS `danhmucsp` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `TENDM` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `danhmucsp`
@@ -49,11 +51,15 @@ INSERT INTO `danhmucsp` (`ID`, `TENDM`, `createdAt`, `updatedAt`) VALUES
 -- Cấu trúc bảng cho bảng `daugia`
 --
 
-CREATE TABLE `daugia` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `daugia`;
+CREATE TABLE IF NOT EXISTS `daugia` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `MASP` int(11) NOT NULL,
   `MAIL` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
-  `GIA` double NOT NULL
+  `GIA` double NOT NULL,
+  `NGDG` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_DauGiaSanPham` (`MASP`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -62,11 +68,14 @@ CREATE TABLE `daugia` (
 -- Cấu trúc bảng cho bảng `ketquadg`
 --
 
-CREATE TABLE `ketquadg` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `ketquadg`;
+CREATE TABLE IF NOT EXISTS `ketquadg` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `MASP` int(11) NOT NULL,
   `MAIL` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
-  `GIA` double NOT NULL
+  `GIA` double NOT NULL,
+  `NGDG` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -75,10 +84,12 @@ CREATE TABLE `ketquadg` (
 -- Cấu trúc bảng cho bảng `loaitk`
 --
 
-CREATE TABLE `loaitk` (
-  `ID` int(11) NOT NULL,
-  `TENLOAI` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `loaitk`;
+CREATE TABLE IF NOT EXISTS `loaitk` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TENLOAI` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `loaitk`
@@ -86,8 +97,7 @@ CREATE TABLE `loaitk` (
 
 INSERT INTO `loaitk` (`ID`, `TENLOAI`) VALUES
 (1, 'ADMIN'),
-(2, 'Người mua hàng'),
-(3, 'Người Bán Hàng');
+(2, 'PERSON');
 
 -- --------------------------------------------------------
 
@@ -95,29 +105,33 @@ INSERT INTO `loaitk` (`ID`, `TENLOAI`) VALUES
 -- Cấu trúc bảng cho bảng `sanpham`
 --
 
-CREATE TABLE `sanpham` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `sanpham`;
+CREATE TABLE IF NOT EXISTS `sanpham` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `TENSP` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `MADM` int(11) NOT NULL,
   `GIAKHOIDIEM` double NOT NULL,
+  `GIABAN` int(11) NOT NULL,
   `MOTA` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
-  `TRANGTHAI` int(11) NOT NULL,
+  `TRANGTHAI` int(11) NOT NULL DEFAULT '2',
   `NGAYBD` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `NGAYKT` datetime NOT NULL,
   `HINH` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  KEY `FK_SanPhamDanhMuc` (`MADM`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `sanpham`
 --
 
-INSERT INTO `sanpham` (`ID`, `TENSP`, `MADM`, `GIAKHOIDIEM`, `MOTA`, `TRANGTHAI`, `NGAYBD`, `NGAYKT`, `HINH`, `createdAt`, `updatedAt`) VALUES
-(1, 'IPhone X', 2, 10000000, 'Điện Thoại IP X', 1, '2018-06-01 10:00:00', '2018-10-17 11:00:00', '', '2018-05-31 17:07:08', '2018-10-01 17:07:08'),
-(2, 'IPhone 8P', 2, 9000000, 'Điện Thoại IP 8 Plus', 1, '2018-06-01 05:00:00', '2018-11-01 04:00:00', '', '2018-05-31 17:07:08', '2018-05-31 17:07:08'),
-(3, 'IPhone 8', 2, 8000000, 'Điện Thoại IP 8', 1, '2018-06-01 19:00:00', '2018-10-01 00:00:00', '', '2018-05-31 17:07:08', '2018-05-31 17:07:08'),
-(4, 'MacBook Pro', 1, 20000000, 'Máy Tính Xách Tay Macbook Pro 15', 1, '2018-06-03 07:00:00', '2018-11-14 00:00:00', '', '2018-05-31 17:07:08', '2018-05-31 17:07:08');
+INSERT INTO `sanpham` (`ID`, `TENSP`, `MADM`, `GIAKHOIDIEM`, `GIABAN`, `MOTA`, `TRANGTHAI`, `NGAYBD`, `NGAYKT`, `HINH`, `createdAt`, `updatedAt`) VALUES
+(1, 'IPhone X', 2, 500000, 20000000, 'Điện Thoại IP X', 1, '2018-06-01 10:00:00', '2018-10-17 11:00:00', 'ipx.jpg', '2018-05-31 17:07:08', '2018-10-01 17:07:08'),
+(2, 'IPhone 8P', 2, 400000, 15000000, 'Điện Thoại IP 8 Plus', 1, '2018-06-01 05:00:00', '2018-11-01 04:00:00', 'ip8p.jpg', '2018-05-31 17:07:08', '2018-05-31 17:07:08'),
+(3, 'IPhone 8', 2, 300000, 10000000, 'Điện Thoại IP 8', 1, '2018-06-01 19:00:00', '2018-10-01 00:00:00', 'ip8.jpg', '2018-05-31 17:07:08', '2018-05-31 17:07:08'),
+(4, 'MacBook Pro', 1, 1000000, 25000000, 'Máy Tính Xách Tay Macbook Pro 15', 1, '2018-06-03 07:00:00', '2018-11-14 00:00:00', 'mbr.jpg', '2018-05-31 17:07:08', '2018-05-31 17:07:08');
 
 -- --------------------------------------------------------
 
@@ -125,17 +139,27 @@ INSERT INTO `sanpham` (`ID`, `TENSP`, `MADM`, `GIAKHOIDIEM`, `MOTA`, `TRANGTHAI`
 -- Cấu trúc bảng cho bảng `taikhoan`
 --
 
-CREATE TABLE `taikhoan` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `taikhoan`;
+CREATE TABLE IF NOT EXISTS `taikhoan` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `HOTEN` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `MAIL` text NOT NULL,
-  `PASSWORD` varchar(10) NOT NULL,
+  `PASSWORD` varchar(100) NOT NULL,
   `DIACHI` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `LOAITK` int(11) NOT NULL,
-  `TINHTRANG` int(11) NOT NULL,
+  `TINHTRANG` int(11) NOT NULL DEFAULT '1',
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  KEY `FK_TaiKhoanLoaiTaiKhoan` (`LOAITK`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `taikhoan`
+--
+
+INSERT INTO `taikhoan` (`ID`, `HOTEN`, `MAIL`, `PASSWORD`, `DIACHI`, `LOAITK`, `TINHTRANG`, `createdAt`, `updatedAt`) VALUES
+(1, 'Evans Ray Kyle', 'ndhoang011097@gmail.com', '617556d92b2c180265653996a4b882a6', 'C2A Buu Long', 2, 1, '2018-06-15 15:41:30', '2018-06-15 15:41:30');
 
 -- --------------------------------------------------------
 
@@ -143,10 +167,12 @@ CREATE TABLE `taikhoan` (
 -- Cấu trúc bảng cho bảng `xntinhtrang`
 --
 
-CREATE TABLE `xntinhtrang` (
-  `ID` int(11) NOT NULL,
-  `TENTT` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `xntinhtrang`;
+CREATE TABLE IF NOT EXISTS `xntinhtrang` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TENTT` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `xntinhtrang`
@@ -155,101 +181,6 @@ CREATE TABLE `xntinhtrang` (
 INSERT INTO `xntinhtrang` (`ID`, `TENTT`) VALUES
 (1, 'Đã Xác Nhận'),
 (2, 'Chưa Xác Nhận');
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `danhmucsp`
---
-ALTER TABLE `danhmucsp`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Chỉ mục cho bảng `daugia`
---
-ALTER TABLE `daugia`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `FK_DauGiaSanPham` (`MASP`);
-
---
--- Chỉ mục cho bảng `ketquadg`
---
-ALTER TABLE `ketquadg`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Chỉ mục cho bảng `loaitk`
---
-ALTER TABLE `loaitk`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Chỉ mục cho bảng `sanpham`
---
-ALTER TABLE `sanpham`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `FK_SanPhamDanhMuc` (`MADM`);
-
---
--- Chỉ mục cho bảng `taikhoan`
---
-ALTER TABLE `taikhoan`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `FK_TaiKhoanLoaiTaiKhoan` (`LOAITK`);
-
---
--- Chỉ mục cho bảng `xntinhtrang`
---
-ALTER TABLE `xntinhtrang`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `danhmucsp`
---
-ALTER TABLE `danhmucsp`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT cho bảng `daugia`
---
-ALTER TABLE `daugia`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `ketquadg`
---
-ALTER TABLE `ketquadg`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `loaitk`
---
-ALTER TABLE `loaitk`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT cho bảng `sanpham`
---
-ALTER TABLE `sanpham`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT cho bảng `taikhoan`
---
-ALTER TABLE `taikhoan`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT cho bảng `xntinhtrang`
---
-ALTER TABLE `xntinhtrang`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
