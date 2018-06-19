0f671e0db3b0aaa65783f2420ad1629bfe10e7cd
Nguyễn Đức Hoàng <ndhoang011097@gmail.com>
2018-06-19 15:05:53 +0700

    Database
    Update

--- qldaugia.sql
+++ qldaugia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th6 15, 2018 lúc 03:55 PM
+-- Thời gian đã tạo: Th6 19, 2018 lúc 08:04 AM
 -- Phiên bản máy phục vụ: 5.7.21
 -- Phiên bản PHP: 5.6.35
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `danhmucsp` (
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `danhmucsp`
@@ -55,12 +55,19 @@ DROP TABLE IF EXISTS `daugia`;
 CREATE TABLE IF NOT EXISTS `daugia` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `MASP` int(11) NOT NULL,
-  `MAIL` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `GIA` double NOT NULL,
   `NGDG` int(11) NOT NULL,
+  `NGAY` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`),
   KEY `FK_DauGiaSanPham` (`MASP`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `daugia`
+--
+
+INSERT INTO `daugia` (`ID`, `MASP`, `GIA`, `NGDG`, `NGAY`) VALUES
+(1, 1, 500000, 1, '2018-06-18 18:42:38');
 
 -- --------------------------------------------------------
 
@@ -72,7 +79,6 @@ DROP TABLE IF EXISTS `ketquadg`;
 CREATE TABLE IF NOT EXISTS `ketquadg` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `MASP` int(11) NOT NULL,
-  `MAIL` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `GIA` double NOT NULL,
   `NGDG` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
@@ -108,6 +114,7 @@ INSERT INTO `loaitk` (`ID`, `TENLOAI`) VALUES
 DROP TABLE IF EXISTS `sanpham`;
 CREATE TABLE IF NOT EXISTS `sanpham` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NGUOIBAN` int(11) NOT NULL,
   `TENSP` text CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `MADM` int(11) NOT NULL,
   `GIAKHOIDIEM` double NOT NULL,
@@ -121,17 +128,18 @@ CREATE TABLE IF NOT EXISTS `sanpham` (
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`),
   KEY `FK_SanPhamDanhMuc` (`MADM`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `sanpham`
 --
 
-INSERT INTO `sanpham` (`ID`, `TENSP`, `MADM`, `GIAKHOIDIEM`, `GIABAN`, `MOTA`, `TRANGTHAI`, `NGAYBD`, `NGAYKT`, `HINH`, `createdAt`, `updatedAt`) VALUES
-(1, 'IPhone X', 2, 500000, 20000000, 'Điện Thoại IP X', 1, '2018-06-01 10:00:00', '2018-10-17 11:00:00', 'ipx.jpg', '2018-05-31 17:07:08', '2018-10-01 17:07:08'),
-(2, 'IPhone 8P', 2, 400000, 15000000, 'Điện Thoại IP 8 Plus', 1, '2018-06-01 05:00:00', '2018-11-01 04:00:00', 'ip8p.jpg', '2018-05-31 17:07:08', '2018-05-31 17:07:08'),
-(3, 'IPhone 8', 2, 300000, 10000000, 'Điện Thoại IP 8', 1, '2018-06-01 19:00:00', '2018-10-01 00:00:00', 'ip8.jpg', '2018-05-31 17:07:08', '2018-05-31 17:07:08'),
-(4, 'MacBook Pro', 1, 1000000, 25000000, 'Máy Tính Xách Tay Macbook Pro 15', 1, '2018-06-03 07:00:00', '2018-11-14 00:00:00', 'mbr.jpg', '2018-05-31 17:07:08', '2018-05-31 17:07:08');
+INSERT INTO `sanpham` (`ID`, `NGUOIBAN`, `TENSP`, `MADM`, `GIAKHOIDIEM`, `GIABAN`, `MOTA`, `TRANGTHAI`, `NGAYBD`, `NGAYKT`, `HINH`, `createdAt`, `updatedAt`) VALUES
+(1, 1, 'IPhone X', 2, 500000, 20000000, 'Điện Thoại IP X', 1, '2018-06-01 10:00:00', '2018-10-17 11:00:00', 'ipx.jpg', '2018-05-31 17:07:08', '2018-10-01 17:07:08'),
+(2, 1, 'IPhone 8P', 2, 400000, 15000000, 'Điện Thoại IP 8 Plus', 1, '2018-06-01 05:00:00', '2018-11-01 04:00:00', 'ip8p.jpg', '2018-05-31 17:07:08', '2018-05-31 17:07:08'),
+(3, 1, 'IPhone 8', 2, 300000, 10000000, 'Điện Thoại IP 8', 1, '2018-06-01 19:00:00', '2018-10-01 00:00:00', 'ip8.jpg', '2018-05-31 17:07:08', '2018-05-31 17:07:08'),
+(4, 1, 'MacBook Pro', 1, 1000000, 25000000, 'Máy Tính Xách Tay Macbook Pro 15', 1, '2018-05-03 07:00:00', '2018-05-11 07:00:00', 'mbr.jpg', '2018-04-30 17:07:08', '2018-05-31 17:07:08'),
+(6, 1, 'Xiaomi Mi4', 2, 300000, 3000000, 'Made in China', 1, '2018-06-19 13:22:26', '2018-06-26 00:00:00', 'mi4.jpg', '2018-06-19 06:22:26', '2018-06-19 06:22:26');
 
 -- --------------------------------------------------------
 
@@ -152,14 +160,15 @@ CREATE TABLE IF NOT EXISTS `taikhoan` (
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`),
   KEY `FK_TaiKhoanLoaiTaiKhoan` (`LOAITK`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `taikhoan`
 --
 
 INSERT INTO `taikhoan` (`ID`, `HOTEN`, `MAIL`, `PASSWORD`, `DIACHI`, `LOAITK`, `TINHTRANG`, `createdAt`, `updatedAt`) VALUES
-(1, 'Evans Ray Kyle', 'ndhoang011097@gmail.com', '617556d92b2c180265653996a4b882a6', 'C2A Buu Long', 2, 1, '2018-06-15 15:41:30', '2018-06-15 15:41:30');
+(1, 'Evans Ray Kyle', 'ndhoang011097@gmail.com', '617556d92b2c180265653996a4b882a6', 'C2A Buu Long', 2, 1, '2018-06-15 15:41:30', '2018-06-15 15:41:30'),
+(2, 'Quản Trị Viên', 'Admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '++++++++++++++++++++++', 1, 1, '2018-06-18 19:25:15', '2018-06-18 19:25:15');
 
 -- --------------------------------------------------------
 
